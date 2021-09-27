6fc86ff3f548e0491e6267fdd548e2b82d2c4987
Antoine Bouffard <antoine.bouffard@ynov.com>
2021-09-27 16:53:22 +0200

    Fix a la con

--- puissance4/src/main/java/puissance4/Systeme.java
+++ puissance4/src/main/java/puissance4/Systeme.java
@@ -11,41 +11,51 @@ class Systeme {
 class Partie {
     Player player1;
     Player player2;
-    Grille grille;
+    Grid grid;
 
-    public Partie(Player player1, Player player2, Grille grille){
+    public Partie(Player player1, Player player2, Grid grid){
         this.player1 = player1;
         this.player2 = player2;
-        this.grille = grille;
+        this.grid = grid;
     }
 
-    private Player Victoire(Grille grille){
+    private Player Victoire(Grid grid){
         return this.player1;
     }
 
-    private Player Defaite(Grille grille){
+    private Player Defaite(Grid grid){
         return this.player2;
     }
 
-    private Boolean Egaliter(Grille grille){
-        return grille.IsFull();
+    private Boolean Egaliter(Grid grid){
+        return Grid.IsFull();
     }
 
     public void Tours(Player player){
-        //reload la grille
+        int PlayerInput = 0;
+        //reload la Grid
         java.io.InputStreamReader isr = new java.io.InputStreamReader(System.in);
         java.io.BufferedReader br = new java.io.BufferedReader(isr);
         try {
-            String FirstName = br.readLine();
+            PlayerInput = Integer.parseInt(br.readLine());
         }catch(IOException e){
             System.err.println(e.getMessage());
         }
+        if (PlayerInput >= 1 && PlayerInput <= this.grid.length){
+            for (String[] x : this.grid.grid){
+                for (String y : x){
+                    if (y != "X" || x != "Y"){
+
+                    }
+                }
+            }
+        }
         // test si il choisi un emplacement valide
         //si oui
-            //placer le symbole du player sur la grille
+            //placer le symbole du player sur la Grid
             //test victoire
                 //si oui fin de la partie 
-                //si non fin du tours et teste grille plein
+                //si non fin du tours et teste Grid plein
         //si non
             //this.Tours(player);
             //afficher un msg d'erreur
